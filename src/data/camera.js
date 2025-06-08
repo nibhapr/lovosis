a84f18279157d01dd3c81ede83c16ff438348075
nibhapr <nibhapr@gmail.com>
2025-06-08 14:00:19 +0530

    first commit

---
+++ src/data/camera.js
@@ -0,0 +1,209 @@
+export const cameraData = [
+  {
+    author: "Gafoor.TC",
+    title: "Uho-P1A-M3F4D",
+    description:"High-quality 3MP, 1/2.8 CMOS sensor delivers 2304×1296@20fps video with Ultra265 and H.26 compression. Supports digital WDR for balanced lighting and smart IR up to 30m plus 10m warm light for versatile scenes. Built-in mic and speaker enable two-way audio for enhanced interaction. Wi-Fi connectivity and easy installation. IP66-rated for weather resistance. Features sound and light alarms for noticeable warnings. Front face made of graphene for superior heat dissipation and eco-friendly design.",
+    image: "/pictures/Uniarch/Uniarch.png",
+  },
+  {
+    author: "Shihabb.p",
+    title: "Uho-S3E-M3",
+    description:
+      "High-quality 3MP, 1/3 CMOS sensor supports multiple resolutions: 3MP@25/20fps, 2MP@30/25fps, and 720P@30/25fps. Supports Ultra265, H.265, and H.264 compression. Built-in mic and speaker enable two-way audio for better interaction. IR night vision up to 10m (33ft). Supports up to 256GB Micro SD storage. Wi-Fi connectivity and easy installation.",
+    image: "/pictures/Uniarch/Uniarch2.png",
+  },
+  {
+    author: "Roshan Aqther",
+    title: "UHO-S2",
+    description:
+      "High-quality 2MP, 1/3 CMOS sensor with 2MP@25/20fps and 720P@25/20fps resolutions. Supports Ultra265, H.265, and H.264 compression. Built-in mic and speaker enable two-way audio for enhanced interaction. IR night vision up to 10m (33ft). Supports up to 256GB Micro SD card. Wi-Fi connectivity and easy installation.",
+    image: "/pictures/Uniarch/Uniarch4.png",
+  },
+  {   
+     author: "Zameel",
+    title: "NVR-104LS-P4",
+    description:
+      "Supports Ultra265, H.265, and H.264 video formats. Features 4-channel input with Plug & Play via 4 independent PoE network interfaces. Compatible with third-party IP cameras through ONVIF (Profiles S, G, T). Offers 1 HDMI and 1 VGA port with simultaneous output. Records up to 5MP resolution. Includes 1 SATA HDD slot supporting up to 6TB storage. Supports cloud firmware upgrades.",
+    image: "/pictures/Uniarch/Uniarch11.png",
+  },
+];
+export const univiewData = [
+  {
+    id: 1,
+    author: "Gafoor.TC",
+    title: "IPC6622SR-X25-VF",
+    description:
+      "High quality image with 2MP,1/2.8’’ CMOS sensor,1920*1080@60fps in the main stream,Ultra 265, H.265, H.264, MJPEG,33X Optical Zoom,Smart intrusion prevention, support false alarm filtering, include Cross Line, Intrusion, Enter Area, Leave Area detection,People Counting, support people flow counting and crowd density monitoring, suitable for different statistical scenarios,Lighthunter technology ensures ultra-high image quality in low illumination environment",
+    image: "/pictures/unv/unv1.png",
+  },
+  {
+    id: 2,
+    author: "Shihabb.p",
+    title: "IPC6658SR-X25-VF",
+    description:
+      "High quality image with 8MP,1/1.8’’ CMOS sensor,3840*2160@30fps in the main stream,Ultra 265, H.265, H.264, MJPEG,25X Optical Zoom,Smart intrusion prevention, support false alarm filtering, include Cross Line, Intrusion, Enter Area, Leave Area detection,People Counting, support people flow counting and crowd density monitoring",
+    image: "/pictures/unv/unv2.png",
+  },
+  {
+    id: 3,
+    author: "Roshan Aqther",
+    title: "IPC6315LFW-AX5C-VG1",
+    description:
+      "High quality image with 5MP, 1/2.8, CMOS sensorUltra 265, H.265, H.264, MJPEG,Smart intrusion prevention, support false alarm filtering, include Cross Line, Intrusion, Enter Area, Leave Area detection,LightHunter technology ensures ultra-high image quality in low illumination environment,Up to 120 dB WDR (Wide Dynamic Range)",
+    image: "/pictures/unv/unv3.png",
+  },
+  {
+    id: 4,
+    author: "Zameel",
+    title: "IPC6424SR-X25-VF-B",
+    description:
+      "High quality image with 4MP,1/2.8’’ CMOS sensor,2688*1520@30fps in the main stream,Ultra 265, H.265, H.264, MJPEG,25X Optical Zoom,Smart intrusion prevention, support false alarm filtering, include Cross Line, Intrusion, Enter Area, Leave Area detection,People Counting, support people flow counting and crowd density monitoring",
+    image: "/pictures/unv/unv4.png",
+  },
+];
+export const LovosisData = [
+  {
+    title: "Authorized UNV Distributor",
+    description:
+      "We are the official distributor of UNV cameras and UNV CCTV systems in Dubai, providing you with direct access to the latest products and innovations",
+    image: "/pictures/icons/icon2.png",
+  },
+  {
+    title: "Wide Range of UNV CCTV Products",
+    description:
+      " Our product range includes everything from Uniview cameras to complete CCTV systems, ensuring that you find the perfect solution for your security needs.",
+    image: "/pictures/icons/icon.png",
+  },
+  {
+    title: "After-Sales Service and Warranty",
+    description:
+      "Customer satisfaction is a top priority for Lovosis Technology. They offer comprehensive after-sales services, including maintenance, repairs, and software updates.",
+    image: "/pictures/icons/sales.png",
+  },
+];
+
+  export const unvData = [
+    {
+      title: "Superior Video Quality:",
+      description:
+        "With its pristine 4K resolution, UNV cameras make sure that every detail is recorded.",
+      image: "/pictures/unv/video.jpg",
+    },
+    {
+      title: "Advanced AI Technology",
+      description:
+        "AI-powered capabilities like motion detection, facial recognition, and intrusion alarms are included in UNV CCTV systems.",
+      image: "/pictures/unv/advanced.jpg",
+    },
+    {
+      title: "Competitive UNV Camera Prices",
+      description:
+        "We provide you with unrivaled value by offering the finest UNV CCTV camera prices in the Middle East because to our established network and bulk purchasing ability.",
+      image: "/pictures/unv/prices.png",
+    },
+    {
+      title: "After-Sales Service and Warranty",
+      description:
+        "Lovosis Technology places a high premium on client happiness. They provide extensive post-purchase services, including as upkeep, fixes, and software upgrades.",
+      image: "/pictures/unv/sales.png",
+    },
+  ];
+  export const hikVision = [
+    {
+      title: "Superior Video Quality:",
+      description:
+        "With its pristine 4K resolution, UNV cameras make sure that every detail is recorded.",
+      image: "/pictures/unv/video.jpg",
+    },
+    {
+      title: "Advanced AI Technology",
+      description:
+        "AI-powered capabilities like motion detection, facial recognition, and intrusion alarms are included in UNV CCTV systems.",
+      image: "/pictures/unv/advanced.jpg",
+    },
+    {
+      title: "Competitive UNV Camera Prices",
+      description:
+        "We provide you with unrivaled value by offering the finest UNV CCTV camera prices in the Middle East because to our established network and bulk purchasing ability.",
+      image: "/pictures/unv/prices.png",
+    },
+    {
+      title: "After-Sales Service and Warranty",
+      description:
+        "Lovosis Technology places a high premium on client happiness. They provide extensive post-purchase services, including as upkeep, fixes, and software upgrades.",
+      image: "/pictures/unv/sales.png",
+    },
+  ];
+export const dahuaData = [
+  {
+    title: "High-Quality Dahua IP Cameras",
+    description:
+      "For those seeking reliable, high-resolution video surveillance, Dahua IP cameras offer exceptional image quality and smart features. Ideal for both commercial and residential settings, Dahua IP cameras integrate seamlessly with your network and offer remote monitoring via smartphones and other devices. Whether you need to keep an eye on your business or secure your home, Dahua IP cameras are the perfect choice.",
+    image: "/pictures/icons/icon45.png",
+  },
+  {
+    title: "Dahua WiFi Cameras: Convenient and Flexible Surveillance",
+    description:
+      "If you need a wireless solution for your surveillance needs, Dahua WiFi cameras are ideal. They provide flexible installation options without the need for extensive wiring. Whether for home security or small businesses, Dahua WiFi cameras offer ease of use, mobility, and reliable performance.",
+    image: "/pictures/icons/icon7.png",
+  },
+  {
+    title: "Competitive Dahua Camera Prices in Dubai",
+    description:
+      "At Lovosis Technology L.L.C, we believe in providing affordable, high-quality Dahua cameras to meet the diverse needs of our customers. Whether you’re looking for a budget-friendly Dahua camera or a premium Dahua CCTV system, we offer competitive Dahua camera prices in the UAE, ensuring that you get the best value for your investment in security.",
+    image: "/pictures/icons/icon6.png",
+  },
+];
+export const unvdisplaydata = [
+  {
+    id: 1,
+    title: "MW3565-U-E2",
+    description:
+      "4K UHD anti-glare display with zero lamination for reduced light refraction and sharper visuals. Ultra-narrow bezel and minimalist geometric design for elegant aesthetics. Optional 4K HD camera and 8-mic array enable immersive collaboration. Dual Wi-Fi and Bluetooth 5.2 ensure seamless connectivity. Dual treble and bass channels deliver cinematic audio. Infrared 50-point touch and Android 13.0 with smart tools enhance interactivity. Supports wireless screen mirroring, fast QR code file transfers, OTA upgrades, and NFC screen projection.",
+    image: "/pictures/Unvdisplay/Unvdisplay.png",
+    author: "Mural Krishna",
+  },
+  {
+    id: 2,
+    title: "MW7815-FI-EB",
+    description:
+      "Advanced grayscale for lifelike detail and seamless image splicing with no black lines. Slim, compact design with a stylish appearance and space-saving installation. High brightness, contrast, and ultra-wide viewing angle ensure vivid visuals from any angle. High refresh rate eliminates ghosting and smearing. Features front maintenance, long lifespan, low failure rate, and minimal maintenance cost. Wide voltage input power supply with strong adaptability. Supports wall mounting without clearance and offers optional dual power and dual receiving card configurations.",
+    image: "/pictures/Unvdisplay/MW2.png",
+    author: "Thomas",
+  },
+  {
+    id: 3,
+    title:"MW7809-FI-CF",
+    description:
+      "COB technology with IP65 front protection ensures waterproof and anti-collision durability. Flip-chip design eliminates wire bonding for stable, energy-efficient, high-luminance performance. Advanced grayscale delivers detailed, realistic images with seamless splicing and no black lines. Modular magnetic suction installation offers a sleek look and easy setup. Supports front maintenance, wall mounting, and ultra-slim space-saving design. Features long lifespan, low heat with excellent dissipation, silent operation, and low failure and maintenance costs.",
+    image: "/pictures/Unvdisplay/MW1.png",
+    author: "Sanam Rehman",
+  },
+];
+export const alhuanewData = [
+  {
+    title: "High-Quality Imaging & Resolution",
+    description:
+      "Dahua cameras deliver exceptional image quality with 4K resolution, ensuring that every detail is captured with clarity. This makes them perfect for monitoring expansive areas as well as small, intricate details like license plates or faces. Their cameras also incorporate low-light technologies, providing clear footage even in difficult lighting conditions. For instance, their Starlight technology significantly enhances performance in low-light environments.",
+    image: "/pictures/3.jpg",
+    
+  },
+  {
+    title: "Advanced Motion & Smart Analytics",
+    description:
+      "Dahua cameras are equipped with advanced analytics features such as face detection, perimeter protection, and line crossing detection. These smart analytics help reduce false alarms and improve the overall efficiency of your surveillance system. Additionally, their cameras utilize AI-driven analytics for enhanced object classification, ensuring that you receive only the most relevant alerts.",
+    image: "/pictures/1.jpg",
+  },
+  {
+    title: "Wide Range of Camera Options",
+    description:
+      "Dahua offers a wide variety of surveillance cameras, including PTZ (Pan-Tilt-Zoom) cameras, bullet cameras, dome cameras, and fisheye cameras, each with unique features to meet different security requirements. They also provide thermal cameras, which are excellent for detecting heat signatures, making them useful in both security and industrial applications.",
+    image: "/pictures/dahua6.png",
+  },
+  {
+    title: "Easy Integration & Scalability",
+    description:
+      "Dahua cameras can seamlessly integrate with other security systems, whether it's an alarm system or access control system. Their open architecture allows for flexible deployment. Whether you need a single camera for a small business or hundreds for a large enterprise, Dahua provides scalable solutions to meet your needs.",
+    image: "/pictures/dahuacctv.png",
+  },
+];
